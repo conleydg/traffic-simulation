1ac97c1d655e3561d2537737dc67bcbeaba89aa3
gcrowder <gregory.j.crowder@gmail.com>
2016-04-14 15:32:43 -0400

    Holy crap, the Vehicle class actually kinda works.

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -1,6 +1,6 @@
 class Road:
     def __init__(self, length=1000):
-        self.length = length - 1
+        self.length = length
         self.road_entrance = 0
 
     def car_loop(self):
@@ -18,9 +18,9 @@ class Vehicle:
     10 percent chance of slowing down 2m/s
     30 cars in simulation, evenly spaced at start.
     """
-    def __init__(self, location=(0, 4)):
+    def __init__(self, location=(0, 4), speed=0):
         self.location = location
-        self.speed = 0
+        self.speed = speed
         self.acceleration = 2.0
         self.size = 5
         self.max_speed = 33.3333
@@ -29,7 +29,7 @@ class Vehicle:
     def move_car(self, next_car):
         space = self.get_space_ahead(next_car)
         self.update_speed(space, next_car)
-        self.set_location(space)
+        self.set_location(next_car)
 
     def get_space_ahead(self, next_car):
         if next_car.location[0] < self.location[1]:
@@ -37,7 +37,7 @@ class Vehicle:
         else:
             return next_car.location[0] - self.location[1]
 
-    def set_location(self):
+    def set_location(self, next_car):
         start = self.location[0] + self.speed
         end = self.location[1] + self.speed
         # (993, 998) --> (998, 1003)
@@ -56,10 +56,17 @@ class Vehicle:
         else:
             self.speed = next_car.speed
 # [0, 0, 0, 1, 1, Car, 1, 1, 0, 0, 0, 0, 1, 1, Car, 1, 1, 0, 0]
+car_list = []
+for number in range(30):
+    car_list.append(Vehicle((number * 10, (number * 10) + 4)))
 
-
-red_car = Vehicle(7, 12)
-
-blue_car = Vehicle(0, 4)
+for number in range(10):
+    print("{}seconds".format(number))
+    for index, car in enumerate(car_list):
+        try:
+            car.move_car(car_list[index + 1])
+        except IndexError:
+            car.move_car(car_list[0])
+        print("{} Car Location: ".format(index), car.location, "{} Car Speed: ".format(index), car.speed)
 
 # [Car, Car, Car, Car, Car]
